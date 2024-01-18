1a8fed693b9b27fd26767baaa1429b5986cfd779
José Antonio Pinto dos Santos <jose.antonio@noahsystems.com.br>
2024-01-17 20:24:57 -0400

    refactor: remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { HttpAdapterHost, NestApplication, NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { TrimStringsPipe } from './modules/common/transformer/trim-strings.pipe';
 import { AppModule } from './modules/main/app.module';
 import { setupSwagger } from './swagger';
-import { PrismaExceptionFilter } from './adapter/prisma.exception.filter';
 
 const logger: Logger = new Logger();
 
@@ -16,8 +15,6 @@ async function bootstrap(): Promise<void> {
   setupSwagger(app);
   app.enableCors();
   app.useGlobalPipes(new TrimStringsPipe(), new ValidationPipe());
-  //const { httpAdapter } = app.get(HttpAdapterHost)
-  //app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter))
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
@@ -28,4 +25,4 @@ async function bootstrap(): Promise<void> {
   logger.log(`NestJS initialized with port(s): ${port} (http)`);
 }
 
-bootstrap();
+bootstrap().catch((error) => logger.log(error));
